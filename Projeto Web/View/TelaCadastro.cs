e1079f2dd139818daae22d7101877d0393128da8
brunasb99 <bruna.sousa.sb@gmail.com>
2022-04-01 10:34:52 -0300

    att

--- Projeto Web/View/TelaCadastro.cs
+++ Projeto Web/View/TelaCadastro.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Projeto_Web.Models;
 
 namespace Projeto_Web
 {
     public partial class TelaCadastro : Form
     {
+        CadastroModel cliente;
         public TelaCadastro()
         {
             InitializeComponent();
@@ -19,7 +21,15 @@ namespace Projeto_Web
 
         private void Salvar(object sender, EventArgs e)
         {
+            cliente = new CadastroModel();
 
+            cliente.Nome = txtNome.Text;
+            cliente.Email = txtEmail.Text;
+            cliente.User = txtUser.Text;
+            cliente.Senha = txtSenha.Text;
+            cliente.Repitasenha = txtRepSenha.Text;
+            ctrlCliente.Salvar(cliente);
+            this.Close();
         }
     }
 }
